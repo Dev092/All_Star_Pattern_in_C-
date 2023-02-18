1b7d69c0749cc392cfda27088c3a3ae53d2669a5
Dev kumar <83683369+Dev092@users.noreply.github.com>
2023-02-18 12:53:57 +0530

    Add files via upload

---
+++ right_angle_triangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b;
+    cin>>b;
+    for(int i=1;i<=b;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
